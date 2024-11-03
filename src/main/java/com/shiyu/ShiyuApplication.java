b3938507dae70fbdf55cb5b4447a451456d41d51
shiyu <876703634@qq.com>
2024-11-03 21:22:01 +0800

    init

---
+++ src/main/java/com/shiyu/ShiyuApplication.java
@@ -0,0 +1,13 @@
+package com.shiyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiyuApplication.class, args);
+    }
+
+}
